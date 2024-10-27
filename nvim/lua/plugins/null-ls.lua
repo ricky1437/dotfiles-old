771f6fe292d758e94532979d8e36bb450fbe4296
rickychann <rickychann9@gmail.com>
2024-10-28 04:30:35 +0900

    Add black for formatting python files

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -5,6 +5,7 @@ return {
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
+                    null_ls.builtins.formatting.black,
                     null_ls.builtins.formatting.prettier.with({
                         filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
                     }),
