d0db6a1831d5a4632822a478301e69acc58c80f3
rickychann <rickychann9@gmail.com>
2024-10-28 03:14:09 +0900

    Set terminal shell to powershell

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,21 @@ vim.g.loaded_netrwPlugin = 0
 vim.g.mapleader = " "
 vim.g.maplocalleader = "<Space>"
 
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+  local pwsh_options = {
+    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for k, v in pairs(pwsh_options) do
+    vim.opt[k] = v
+  end
+end
+
 require("config.vim-option")
 require("config.lazy")
 require("config.lsp")
